6c83d5ecad9c5223cb216a61547c4f512014c90a
Kimberly <buttigieg.kimberly@gmail.com>
2023-12-06 16:47:22 +0100

    Updates to website

--- projects/weather-comparison/script.js
+++ projects/weather-comparison/script.js
@@ -44,12 +44,80 @@ window.addEventListener('load', callAPIs);
 const updateIcon = (weatherCode, day, side) => {
   var iconName;
   switch (weatherCode) {
+    case 0:
+    case 1:
+    case 2:
+      if (day === 0) {
+        iconName = 'moon';
+      } else {
+        iconName = 'sun';
+      }
+      break;
     case 3:
       if (day === 0) {
         iconName = 'cloud-moon';
       } else {
         iconName = 'cloud-sun';
       }
+      break;
+    case 40:
+    case 41:
+    case 42:
+    case 43:
+    case 44:
+    case 45:
+    case 46:
+    case 47:
+    case 48:
+    case 49:
+      iconName = 'cloud-fog';
+      break;
+    case 50:
+    case 51:
+    case 52:
+    case 53:
+    case 54:
+    case 55:
+    case 56:
+    case 57:
+    case 58:
+    case 59:
+      iconName = 'cloud-drizzle';
+      break;
+    case 60:
+    case 61:
+    case 62:
+    case 63:
+      iconName = 'cloud-rain';
+      break;
+    case 64:
+    case 65:
+      iconName = 'cloud-rain-heavy';
+      break;
+    case 66:
+    case 67:
+      iconName = 'cloud-hail';
+      break;
+    case 68:
+    case 69:
+      iconName = 'cloud-sleet';
+      break;
+    case 70:
+    case 71:
+    case 72:
+    case 73:
+    case 74:
+    case 75:
+    case 76:
+    case 77:
+    case 78:
+      iconName = 'cloud-snow';
+      break;
+    case 79:
+      iconName = 'cloud-hail';
+      break;
+    default:
+      iconName = 'question-lg';
   }
   document.getElementById('icon' + side).classList.add('bi-' + iconName);
 };
